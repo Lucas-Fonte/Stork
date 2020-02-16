19721d7509731447962279c533c3c43988bde3f7
Lucas Souza da Fonte <fontelucas@yahoo.com.br>
2020-02-16 20:24:30 -0300

    first layered version of app

---
+++ src/components/InterestRateCalculator/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function InterestRateCalculator() {
+  return (
+    <h1>InterestRateCalculator</h1>
+  );
+}
---
+++ src/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font: 14px 'Roboto', sans-serif;
+  color: #000;
+`;
