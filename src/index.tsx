920860b3bf214494d28cc654d6d7f7e9b9d58893
Lucas Souza da Fonte <fontelucas@yahoo.com.br>
2020-02-17 00:38:58 -0300

    first attempt to turning it into a pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
+import 'dotenv/config';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { registerServiceWorker } from './serviceWorker';
 
+registerServiceWorker();
 ReactDOM.render(<App />, document.getElementById('root'));
