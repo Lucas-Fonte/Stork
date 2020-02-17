35304466c812f6352be55850c1e39c1b67edfe9b
Lucas Fonte <fontelucas@yahoo.com.br>
2020-02-17 18:37:40 +0000

    testing deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { registerServiceWorker } from './serviceWorker';
 
-registerServiceWorker();
 ReactDOM.render(<App />, document.getElementById('root'));
