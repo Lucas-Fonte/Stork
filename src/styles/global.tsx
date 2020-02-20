dd07040c67475cdf7dfb8095e4b93984eee8714e
Lucas Fonte <fontelucas@yahoo.com.br>
2020-02-20 17:43:47 +0000

    testing status bar safe area

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 export default createGlobalStyle`
     * {
         margin: 0;
-        padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left);
+        padding: 0;
         outline: 0;
         box-sizing: border-box;
     }
